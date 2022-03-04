1f4f907e8b5d016daafcf5dc273434d04367c09c
timattt <timattt@gmail.com>
2022-03-04 13:15:01 +0300

    Create Euler.c

---
+++ Problems/EulerNumber/Euler.c
@@ -0,0 +1,55 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define N 21
+
+int main(int argc, char *argv[]) {
+        int commsize = 0;
+        int my_rank = 0;
+        long long i = 0;
+
+        MPI_Init(&argc, &argv);
+        MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+        printf("Process %d started. Total %d processes.\n", my_rank, commsize);
+
+        long long startN = my_rank * N / commsize;
+        long long endN = (my_rank + 1) * N / commsize;
+
+        long double res = 0;
+        long long fac = 1;
+
+        for (i = startN; i < endN; i++) {
+                printf("Process %d fac: %d\n", my_rank, fac);
+                if (i != 0) {
+                        fac *= i;
+                }
+                res += (1.0 / (long double)(fac));
+        }
+        printf("Process %d final fac: %d\n", my_rank, fac);
+
+        long double prevRes = 0;
+        long long prevFac = 1;
+
+        if (my_rank != 0) {
+                MPI_Recv(&prevRes, 1, MPI_LONG_DOUBLE, my_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(&prevFac, 1, MPI_LONG_LONG, my_rank - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("Process %d received prevFact = %d\n", my_rank, prevFac);
+        }
+
+        res /= (long double) prevFac;
+        fac *= prevFac;
+        res += prevRes;
+
+        if (my_rank != commsize - 1) {
+                MPI_Send(&res, 1, MPI_LONG_DOUBLE, my_rank + 1, 0, MPI_COMM_WORLD);
+                MPI_Send(&fac, 1, MPI_LONG_LONG, my_rank + 1, 1, MPI_COMM_WORLD);
+                printf("Process %d is sending fact = %d\n", my_rank, fac);
+        } else {
+                printf("Result: %LF\n", res);
+        }
+
+        MPI_Finalize();
+        return 0;
+}
